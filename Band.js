5c4be3c0332322430ea8d86e3a955679eef85319
dantaayy <dantaayy@gmail.com>
2022-06-30 11:27:50 -0700

    updated db.js

--- Band.js
+++ Band.js
@@ -7,9 +7,9 @@ const {Sequelize, sequelize} = require('./db');
 This will involve calling the sequelize.define() method, and passing in two arguments to the method - the first one is a string 'band', the second is an object with two kep/values pairs.  They are pair1: name, with the value1: Sequelize.STRING, and key2: genre and value2: Sequelize.STRING
 
 */
-const Musician = sequelize.define('musician', {
+const Band = sequelize.define('band', {
     name: Sequelize.STRING,
-    instrument: Sequelize.STRING
+    genre: Sequelize.STRING
 });
 
 module.exports = {
